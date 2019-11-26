05bc15bca18bc1c2e1cb49efc62754ecfcd7ac3f
kongdedong <373303576@qq.com>
2019-11-26 16:25:08 +0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kdd\Desktop\PyCharm_project\devops_dba\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
